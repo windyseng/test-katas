2e71b9045231dce1abf62b2f28101a7a18c3aa95
WINDY SENGSATHEUANE <windy@WINDYs-MacBook-Pro.local>
2019-10-03 11:07:27 -0400

    test files

--- wrap.js
+++ wrap.js
@@ -1,10 +1,7 @@
 const wrap = (line, maxLen) => {
 let returnedStr = "";
-if(line === "") {
-  returnedStr = "";
+if(line === "") return returnedStr;
 }
-console.log(returnedStr);
-}
-//console.log(wrap("",10));
+wrap("",10);
 
 module.exports = wrap;
