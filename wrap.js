d3d8565541f8978f91df27daee8097adab4d897b
WINDY SENGSATHEUANE <windy@WINDYs-MacBook-Pro.local>
2019-10-03 10:52:00 -0400

    test driven katas

---
+++ wrap.js
@@ -0,0 +1,10 @@
+const wrap = (line, maxLen) => {
+let returnedStr = "";
+if(line === "") {
+  returnedStr = "";
+}
+console.log(returnedStr);
+}
+//console.log(wrap("",10));
+
+module.exports = wrap;
