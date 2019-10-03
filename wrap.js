6c7f88a4efead43e006c1db5450ae135492d8a1b
WINDY SENGSATHEUANE <windy@WINDYs-MacBook-Pro.local>
2019-10-03 12:04:37 -0400

    completed first kata

--- wrap.js
+++ wrap.js
@@ -1,7 +1,41 @@
-const wrap = (line, maxLen) => {
-let returnedStr = "";
-if(line === "") return returnedStr;
+function wrap(line, maxLen){
+  if(line.length <= maxLen){
+    return line;
+  }
+  const indexOfBlank = line.lastIndexOf(' ', maxLen);
+  let split;
+  let offset;
+  if(indexOfBlank > -1){
+    split = indexOfBlank;
+    offset = 1;
+  }else{
+    split=maxLen;
+    offset=0;
+  }
+  return line.substring(0, split) +  "\n" + wrap(line.substring(split + offset), maxLen)
 }
-wrap("",10);
+console.log(wrap("this exercise is weird", 10));
+
+// const wrap = (line, maxLen) => {
+// let returnedStr = "";
+// let count = 0;
+// let newLine = "";
+// if(line === "") return returnedStr;
+// const arrStr = line.split(' ')
+// for(let i = 0; i < arrStr.length; i++){
+//   let currentWord = arrStr[i]
+//   for(let j = 0; j < currentWord.length; j++){
+//     if(currentWord.length < maxLen && count < maxLen && currentWord.length <= (maxLen - count)){
+//       count += currentWord.length;
+//       newLine + currentWord;
+//     } else {
+//       newLine + '\n';
+//       count = 0;
+//     }
+//   }
+// }
+// return newLine;
+// }
+// console.log(wrap("this exercise is weird", 10));
 
 module.exports = wrap;
--- wrap.spec.js
+++ wrap.spec.js
@@ -5,4 +5,7 @@ describe('wrap', () => {
   it('Returns empty string if empty string was provided', () => {
     expect(wrap("", 10)).to.equal("");
   });
+  it('Returns a string with length that is less or equal to the max length', () => {
+    expect(wrap("this exercise is weird", 10)).to.equal("this\nexercise\nis weird")
+  });
 });
